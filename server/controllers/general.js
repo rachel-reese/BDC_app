178b6eb33fe0794618cefae86c92e2a7069a0ba3
Suprabhat Rijal <goodguysman@gmail.com>
2023-05-09 10:48:14 -0400

    Latest build with working status api

--- server/controllers/general.js
+++ server/controllers/general.js
@@ -1,13 +1,39 @@
+import axios from "axios";
+import dotenv from 'dotenv';
+dotenv.config();
+const ATMO_KEY = process.env.ATMO_KEY;
+
+async function getDevices() {
+  const config = {
+    method: 'GET',
+    url: `https://api.atmocube.app/v1/public/devices/`,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Authorization': `APIKey ${ATMO_KEY}`,
+    }
+  }
+
+  const devices = await axios.request(config);
+
+  return devices;
+}
+
 const getDeviceStatus = (req, res) => {
   const deviceID = req.query.deviceID;
-  console.log(req.body)
-  const status = {
-    124677080: "inactive",
-    10395607: "active",
-    6045604: "inactive",
-    45668756: "active",
+
+   getDevices().then((response) => {
+
+    const device_word = {}
+    for (let [idx, device] of response.data.entries()){
+
+      if ([device.id == deviceID]){
+        res.send({status: device.status})
+      }
+    }
+    res.send({status: "not found"})
+
+  }) 
+  
   };
-  res.send({status: status[deviceID]});
-};
 
 export { getDeviceStatus };
